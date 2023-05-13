4d836f77c2d38bc16394cf5783e0b7d291f35545
OswaldoGonzalezS <117325223+OswaldoGonzalezS@users.noreply.github.com>
2023-05-13 22:12:05 +0000

    Modfiicacion

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -4,6 +4,15 @@ public class App
 {
     public static void main( String[] args )
     {
-        
+        Person persona1 = new Person("Pedro", " Picadiedras");
+        persona1.introduceYourself(persona1);
+        Person persona2 = new Person("Pablo", " Marmol");
+        persona2.introduceYourself(persona2);
+        Person persona3 = new Person("Vilma", " Picadiedras");
+        persona3.introduceYourself(persona3);
+        Person persona4 = new Person("Betty", " Marmol");
+        persona4.introduceYourself(persona4);
+
+        System.out.println("Cuatro personas creadas");
     }
 }
--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/Person.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/Person.java
@@ -1,3 +1,33 @@
+package com.linkedin.javacodechallenges;
+
 public class Person {
+  String firstName;
+  String lastName;
+
   
+  public Person() {
+  }
+
+  public Person(String firstName, String lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    System.out.println("creado ==> " + this.firstName);
+
+  }
+  public String getFirstName() {
+    return firstName;
+  }
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+  public String getLastName() {
+    return lastName;
+  }
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+  public void introduceYourself (Person p1){
+    System.out.println("Hola soy: " + this.firstName + "he sido creado");
+
+  }
 }
