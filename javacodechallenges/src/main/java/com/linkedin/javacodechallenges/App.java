7db64bf39fb46c915c9e7fcbf0cd253b57c8ceb5
OswaldoGonzalezS <117325223+OswaldoGonzalezS@users.noreply.github.com>
2023-05-15 22:35:21 +0000

    Calculo100Dios y cinco meses

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,13 +1,22 @@
 package com.linkedin.javacodechallenges;
+import java.time.LocalDate;
+import java.util.*;
+
 
 public class App 
 {
     // Create function to calculate the date that's 
     // 100 days from now
-
+    public static LocalDate calcFechaMasCien(){
+    LocalDate fechaActual = LocalDate.now();
+    LocalDate fechaNueva = fechaActual.plusDays(100);
+    LocalDate fechaNuev1 = fechaActual.plusMonths(3);
+    return fechaNueva;
+    };
     public static void main( String[] args )
     {
-        System.out.println("100 days from now is... " 
-            /* add function call */);
+        System.out.println("100 days from now is... " + calcFechaMasCien());
+        
+            /* add function call */;
     }
 }
